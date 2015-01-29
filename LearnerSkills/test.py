5a493d9e308da4d718836ee57057cc69e3876b33
Dhruv choudhary <choudharydhruv@gmail.com>
2015-01-29 02:03:27 -0800

    Latest updates

--- LearnerSkills/test.py
+++ LearnerSkills/test.py
@@ -13,6 +13,12 @@ from dateutil.parser import parse
 import nltk
 from nltk.stem import porter
 
+import scipy.cluster.vq as vq
+
+cent, label = vq.kmeans2(X,k)
+
+
+
 X = np.random.randint(0,9,size=(10))
 Y = np.random.randint(0,9,size=(10))
 
@@ -185,5 +191,50 @@ print reader.groupby('x1').sum().index
 print reader.groupby('x1').sum().values
 #Use size() instead of sum if u dont need to sum
 
+#pandas to numpy
+ar = np.array(df['x1'])
+df = pd.DataFrame(ar,columns=['x1'])
+df = ps.concat([df1,df2], axis=1)
+
+
+df.fillna('')
+
+#libsvm 3.12 wierd rules
+from svm import *
+from svmutil import *
+px= svm_problem(Y,Data.tolist())
+m = svm_train(px, '-t 2 -c 10')
+labels,_,_ = svm_predict(Y_val, Data_val.tolist(), m)
+
+
+"""
+...
+'options':
+    -s svm_type : set type of SVM (default 0)
+        0 -- C-SVC
+        1 -- nu-SVC
+        2 -- one-class SVM
+        3 -- epsilon-SVR
+        4 -- nu-SVR
+    -t kernel_type : set type of kernel function (default 2)
+        0 -- linear: u'*v
+        1 -- polynomial: (gamma*u'*v + coef0)^degree
+        2 -- radial basis function: exp(-gamma*|u-v|^2)
+        3 -- sigmoid: tanh(gamma*u'*v + coef0)
+        4 -- precomputed kernel (kernel values in training_set_file)
+    -d degree : set degree in kernel function (default 3)
+    -g gamma : set gamma in kernel function (default 1/num_features)
+    -r coef0 : set coef0 in kernel function (default 0)
+    -c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)
+    -n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5)
+    -p epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1)
+    -m cachesize : set cache memory size in MB (default 100)
+    -e epsilon : set tolerance of termination criterion (default 0.001)
+    -h shrinking : whether to use the shrinking heuristics, 0 or 1 (default 1)
+    -b probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0)
+    -wi weight : set the parameter C of class i to weight*C, for C-SVC (default 1)
+    -v n: n-fold cross validation mode
+    -q : quiet mode (no outputs)
+"""
 
 
